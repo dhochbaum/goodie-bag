f0a903b0330319030b88db135f7e6d828288c78c
dhochbaum <dhochbaum@gmail.com>
2019-08-24 15:06:19 -0400

    wrote reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,41 @@
+import thunk from 'redux-thunk'
+import axios from 'axios'
+import { get } from 'https';
 
-const initialState = {}
+// Action Types
+const LOAD_ALL_CANDIES = 'LOAD_ALL_CANDIES'
+
+// Action Creators
+export const loadAllCandies = (candies) => ({
+  type: LOAD_ALL_CANDIES,
+  candies
+})
+
+// Thunk Creators
+export const loadAllCandiesThunk = () => {
+  return async dispatch => {
+    try {
+      const {data} = await axios.get('/api/candies')
+      dispatch(loadAllCandies(data))
+    } catch (err) {
+      console.log('Error fetching candies', err)
+    }
+  }
+}
+
+
+
+
+const initialState = { candies: [] }
 
 const rootReducer = (state = initialState, action) => {
   switch (action.type) {
+    case LOAD_ALL_CANDIES: {
+      return {
+        ...state,
+        candies: action.candies
+      }
+    }
     default:
       return state
   }
